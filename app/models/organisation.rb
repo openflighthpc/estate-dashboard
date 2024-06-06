4b3925ec28325923669d2edbcb999a31c4847387
Matt Ruddy <matt@calcwise.com>
2024-06-06 16:59:03 +0100

    Enforce uniqueness for org names

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,6 +1,8 @@
 class Organisation < ApplicationRecord
   has_many :resources, dependent: :destroy
 
+  validates :name, uniqueness: true
+
   def pretty_display
     "'#{self.name}':\n\tSlack Channel Name: #{self.channel_name}\n\tSlack Channel ID: #{self.channel_id}"
   end
