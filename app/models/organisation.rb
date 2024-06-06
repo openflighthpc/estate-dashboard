34293ffaba25723a060ddc0bb09de6efa8e0fc29
Matt Ruddy <matt@calcwise.com>
2024-06-06 15:50:19 +0100

    Implement relations for models

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,4 +1,5 @@
 class Organisation < ApplicationRecord
+  has_many :resources, dependent: :destroy
 
   def pretty_display
     "'#{self.name}':\n\tSlack Channel Name: #{self.channel_name}\n\tSlack Channel ID: #{self.channel_id}"
