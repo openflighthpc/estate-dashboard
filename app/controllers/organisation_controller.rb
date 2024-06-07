b0d5c3a2d5f67ded97453723a33e0be9e45a1b90
Matt Ruddy <matt@calcwise.com>
2024-06-07 15:25:21 +0100

    Implement slack-based requests

---
+++ app/controllers/organisation_controller.rb
@@ -0,0 +1,25 @@
+class OrganisationController < ApplicationController
+
+  def show
+    @org = Organisation.find_by(name: params[:org_name])
+  end
+
+  def send_message
+    org = Organisation.find(params["org_id"])
+    res = org.resources.find_by(id: params["resource_id"])
+
+    msg = ""
+    params.each do |key, value|
+      if !['resource_id', 'controller', 'action', 'org_id'].include?(key) && (res[key].to_s != value) && !value.blank?
+        msg << "\n\t*#{key.humanize(keep_id_suffix: true)}:* #{res[key]} -> *#{value}*"
+      end
+    end
+    if msg.blank?
+      puts "No changes requested"
+    else
+      msg = "-" * 30 + "\nChange request received from *#{org.name}* for *resource ##{res.id}*:" + msg
+      org.send_message(msg)
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root "home#index"
+
+  get "org/:org_name",  to: "organisation#show"
+  post "send-message/", to: "organisation#send_message"
 end
