d9f5c6481dcb13cd38ee91cf28adf0ba43f9ce9e
Stu Franks <stu@stuts.uk>
2024-06-13 11:12:05 +0100

    Move change request page to it's own place

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "home#index"
 
   get "org/:org_name",            to: "organisation#show"
+  get "org/:org_name/change",     to: "organisation#change"
   get "org/:org_name/raw-data",   to: "organisation#raw_data"
   post "send-message/",           to: "organisation#send_message"
 end
